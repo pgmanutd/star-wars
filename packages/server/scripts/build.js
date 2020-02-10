8fd9cdb92abd37e05c3079867a92ce171685738e
Prashant Goel <prashantmanunited@gmail.com>
2020-02-10 11:24:54 +0530

    build: add heroku-postbuild script

--- packages/server/scripts/build.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const execSh = require('exec-sh');
-
-execSh(['cross-env NODE_ENV=production babel src --out-dir build']);
--- packages/server/scripts/serve.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const execSh = require('exec-sh');
-const { resolve } = require('path');
-
-execSh([`node ${resolve('./scripts/build')}`, `node ${resolve('./build')}`]);
